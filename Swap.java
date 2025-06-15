cf10c33fded754ea800c214fbea4f96c9683c235
AnkithShetty936 <rashetty10@gmail.com>
2025-06-15 16:25:26 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,20 @@
+public class Swap {
+    //int a,b,temp;
+    /*public int functionSwap(int a, int b){
+         a=5;
+         b=3;
+         temp=a;
+         a=b;
+         b=temp;
+    }*/
+    public static void main(String[] args){
+        int a,b,temp;
+        a=5;
+        b=3;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("Value for a="+a);
+        System.out.println("Value for b="+b);
+    }
+}
